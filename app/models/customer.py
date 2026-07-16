b1b54c80cef6a6da6e98d88d726dc9c7ac4166ea
Omendra Puri <omendra8285@gmail.com>
2026-07-17 02:44:48 +0530

    Configure Alembic to use application settings

---
+++ app/models/customer.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Boolean
+from sqlalchemy import Numeric
+from sqlalchemy import String
+
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from app.database.base import Base
+from app.models.base_model import TimestampMixin
+
+
+class Customer(TimestampMixin, Base):
+    __tablename__ = "dim_customer"
+
+    customer_key: Mapped[int] = mapped_column(primary_key=True)
+
+    customer_id: Mapped[str] = mapped_column(
+        String(30),
+        unique=True,
+        nullable=False,
+    )
+
+    full_name: Mapped[str] = mapped_column(
+        String(150),
+        nullable=False,
+    )
+
+    gender: Mapped[str] = mapped_column(String(20))
+
+    occupation: Mapped[str] = mapped_column(String(100))
+
+    annual_income: Mapped[float] = mapped_column(
+        Numeric(15, 2)
+    )
+
+    kyc_status: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+    )
+
+    risk_category: Mapped[str] = mapped_column(
+        String(20),
+        default="LOW",
+    )
